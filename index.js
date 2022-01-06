e6ee43fe3a6e36d1ddb3aab2548fa3f971c1a9c3
Eduardo Rahal <eduardorahal@gmail.com>
2022-01-06 12:49:42 -0300

    API Pronta4

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 const app = express();
-const PORT = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 import { pesquisaCPF } from "./routes/cpf.js";
 
-app.listen(PORT, () => {
-    console.log(`Listening at http://localhost:${PORT}`)
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
   });
 
 
